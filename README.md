6122c10452c04a5eef57db5ca2f0900a53730c53
Dosenpfand <m@sad.bz>
2022-04-07 15:49:04 +0200

    Teachers can only see students from same class

--- README.md
+++ README.md
@@ -10,14 +10,11 @@ flask run
 ```
 
 ## TODO
-- [ ] User Statisiken (Anzahl Fragen gesamt (inklusive Gruppen), Anzahl richtig, Reset, oder: extra Tabelle tries mit ref zu user und question?)
+- [ ] User Statistiken (Anzahl Fragen gesamt (inklusive Gruppen), Anzahl richtig, Reset, oder: extra Tabelle tries mit ref zu user und question?)
 - [ ] Auswahl spezifische Ausgabe aus einer Liste, oder aus Topic
 - [ ] User Gruppen (Klassen), Überblick über Ergebnisse für Lehrer
 - [ ] Schüler: Typ (2 aus 5, etc.) Random, Inhaltsgebiet (Algebra 1, Algebra 2, etc.) auswählbar (z.B. Schüler wählt aus Inhaltsgebiet "Wahrscheinlichkeitsrechnung" und Typ)
-- [ ] User Registierung: Rechte-Vergabe, Views, etc.
 - [ ] DB migration
 - [ ] View multiple tables: `CREATE VIEW alle3 as SELECT external_id, topic_id, 'a' FROM question1of6 UNION SELECT external_id, topic_id, 'b' FROM question2of5`
-- [ ] All teachers can see all users
-- [ ] Test flow, after finished one task -> next task, or return to list
 - [ ] Detailed user statistics: Which questions correct/incorrect answered -> repeat incorrect, statistic per topic
 - [ ] Selbstkontrolle: Nächste Aufgabe gibt immer random
--- app/sec_views.py
+++ app/sec_views.py
@@ -1,9 +1,28 @@
 from flask_appbuilder.security.views import UserDBModelView
 from flask_babel import lazy_gettext
+from flask_appbuilder.models.sqla.filters import FilterInFunction
+from flask import g
+from logging import warn
+from . import db
+from .sec_models import ExtendedUser
+from config import AUTH_ROLE_ADMIN
+
+
+def get_learning_groups():
+    if AUTH_ROLE_ADMIN in [role.name for role in g.user.roles]:
+        results = db.session.query(ExtendedUser).distinct(ExtendedUser.learning_group).group_by(ExtendedUser.learning_group).all()
+        learning_groups = [result.learning_group for result in results]
+    else:
+        learning_groups = [g.user.learning_group]
+
+    return learning_groups
 
 
 class ExtendedUserDBModelView(UserDBModelView):
-    label_columns = {'username': 'Benutzername', 'learning_group': 'Klasse', 'tried_questions': 'Gelöste Aufgaben', 'correct_questions': 'Richtig gelöste Aufgaben', 'first_name': 'Vorname', 'last_name': 'Nachname', 'email': 'E-Mail'}
+    base_filters = [['learning_group', FilterInFunction, get_learning_groups]]
+
+    label_columns = {'username': 'Benutzername', 'learning_group': 'Klasse', 'tried_questions': 'Gelöste Aufgaben',
+                     'correct_questions': 'Richtig gelöste Aufgaben', 'first_name': 'Vorname', 'last_name': 'Nachname', 'email': 'E-Mail'}
 
     show_fieldsets = [
         (lazy_gettext('User info'),
