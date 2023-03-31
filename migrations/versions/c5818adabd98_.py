37f80fcebfeea5b7cd7c7e4ca74acc29c6a85546
Dosenpfand <m@sad.bz>
2023-03-31 19:31:41 +0200

    Add merge

---
+++ migrations/versions/c5818adabd98_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c5818adabd98
+Revises: 6d86b42bc14a, 7205b68fa35b
+Create Date: 2023-03-31 19:30:49.959799
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "c5818adabd98"
+down_revision = ("6d86b42bc14a", "7205b68fa35b")
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
