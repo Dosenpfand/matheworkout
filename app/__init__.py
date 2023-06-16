068ebd6bcd9a08864347bc91f2dc79faccbd2c42
Dosenpfand <m@sad.bz>
2023-06-16 15:24:22 +0200

    Add initializer for all achievements

--- app/__init__.py
+++ app/__init__.py
@@ -18,6 +18,7 @@ appbuilder = AppBuilder()
 migrate = Migrate()
 toolbar = DebugToolbarExtension()
 
+
 def create_app(config="config"):
     logging.basicConfig(format="%(asctime)s:%(levelname)s:%(name)s:%(message)s")
     logging.getLogger().setLevel(logging.WARNING)
@@ -54,11 +55,87 @@ def create_app(config="config"):
         # Init achievements
         # TODO: move to config?
         achievements = [
-            Achievement(name="beginner", title="Anfänger", description="Eine Frage richtig beantwortet")
+            Achievement(
+                name="beginner",
+                title="Anfänger(in)???",
+                description="Eine Aufgabe richtig beantwortet",
+            ),
+            Achievement(
+                name="intermediate",
+                title="Fortgeschritten???",
+                description="100 Aufgaben richtig beantwortet",
+            ),
+            Achievement(
+                name="pro",
+                title="Profi???",
+                description="100 Aufgaben richtig beantwortet",
+            ),
+            Achievement(
+                name="bad-luck",
+                title="Pechsträhne",
+                description="5 Aufgaben nacheinander falsch beantwortet",
+            ),
+            Achievement(
+                name="board",
+                title="Bereit für die Matura",
+                description="Alle Aufgaben einer Matura richtig beantwortet",
+            ),
+            Achievement(
+                name="brain",
+                title="Mathematik durchgespielt",
+                description="Alle Aufgaben richtig beantwortet",
+            ),
+            Achievement(
+                name="first",
+                title="Klassenbeste(r)",
+                description="Platz 1 in deiner Klasse, gewertet nach richtig beantworteten Fragen",
+            ),
+            Achievement(
+                name="third",
+                title="Klassenpodest",
+                description="Unter den Top 3 in deiner Klasse, gewertet nach richtig beantworteten Fragen",
+            ),
+            Achievement(
+                name="night-owl",
+                title="Nachteule",
+                description="10 Aufgaben zu später Stunde beantwortet",
+            ),
+            Achievement(
+                name="nth-root",
+                title="Wurzelente",
+                description="Eine Aufgabe als erstes richtig beantwortet",
+            ),
+            Achievement(
+                name="infinity-rat",
+                title="Matheratte",
+                description="Alle Aufgaben einer Matura als erstes richtig beantwortet",
+            ),
+            Achievement(
+                name="see-no-evil",
+                title="Selbstüberschätzung",
+                description="5 Selbstkontrolle-Aufgaben hintereinander richtig beantwortet",
+            ),
+            Achievement(
+                name="speed",
+                title="Marathon",
+                description="50 Aufgaben an einem Tag richtig beantwortet",
+            ),
+            Achievement(
+                name="star",
+                title="Top 1 %",
+                description="Unter den Top 1 Prozent auf matheworkout, gewertet nach richtig beantworteten Aufgaben",
+            ),
+            Achievement(
+                name="math",
+                title="Alter Hase",
+                description="Übt schon länger als ein Jahr",
+            ),
         ]
 
         for achievement in achievements:
-            result = db.session.query(Achievement).filter_by(name=achievement.name).first()
+            result = (
+                db.session.query(Achievement).filter_by(name=achievement.name).first()
+            )
             if not result:
                 db.session.add(achievement)
             else:
