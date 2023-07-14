18e554395af8a3f50dfd0d27bd02178cef624531
Dosenpfand <m@sad.bz>
2023-07-14 14:04:15 +0200

    Add create user

--- prestart.sh
+++ prestart.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-flask db upgrade || flask fab create-db
+flask db upgrade \
+    || ( flask fab create-db \
+        && lask fab create-admin --username admin --firstname admin --lastname admin --email admin@matheworkout.at --password password )
