0fb66defa52cf2194e09ee7be62df2f069df2aab
Dosenpfand <m@sad.bz>
2023-07-14 14:09:02 +0200

    Fix admin creation

--- prestart.sh
+++ prestart.sh
@@ -2,4 +2,4 @@
 
 flask db upgrade \
     || ( flask fab create-db \
-        && lask fab create-admin --username admin --firstname admin --lastname admin --email admin@matheworkout.at --password password )
+        && flask fab create-admin --username admin --firstname admin --lastname admin --email admin@matheworkout.at --password password )
