706aca81a49691fd12f3cca456be9558e062f0ac
Dosenpfand <m@sad.bz>
2023-08-01 16:08:14 +0200

    Split question count into school types

--- app/views/index.py
+++ app/views/index.py
@@ -1,7 +1,7 @@
 from flask_appbuilder import IndexView, expose
 
 from app import db
-from app.models.general import Question, Category
+from app.models.general import Question, Category, SchoolType, Topic
 
 
 class ExtendedIndexView(IndexView):
@@ -10,12 +10,32 @@ class ExtendedIndexView(IndexView):
     @expose("/")
     def index(self):
         self.update_redirect()
-        question_count = db.session.query(Question).count()
-        matura_count = db.session.query(Category).count()
+        question_count_ahs = (
+            db.session.query(Question)
+            .join(Topic)
+            .filter(Topic.school_type == SchoolType.ahs)
+            .count()
+        )
+        print(SchoolType.ahs)
+        print(db.session.query(Question).first())
+        question_count_bhs = (
+            db.session.query(Question)
+            .join(Topic)
+            .filter(Topic.school_type == SchoolType.bhs)
+            .count()
+        )
+        matura_count_ahs = (
+            db.session.query(Category).filter_by(school_type=SchoolType.ahs).count()
+        )
+        matura_count_bhs = (
+            db.session.query(Category).filter_by(school_type=SchoolType.bhs).count()
+        )
         return self.render_template(
             self.index_template,
             appbuilder=self.appbuilder,
-            question_count=question_count,
-            matura_count=matura_count,
+            question_count_ahs=question_count_ahs,
+            question_count_bhs=question_count_bhs,
+            matura_count_ahs=matura_count_ahs,
+            matura_count_bhs=matura_count_bhs,
             title="Willkommen",
         )
