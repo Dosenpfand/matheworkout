1a9d7ccaf12c0bcbd9a54c3c492f0f074025570f
Dosenpfand <git@sad.bz>
2024-10-13 19:44:10 +0200

    Fix deps

--- prestart.sh
+++ prestart.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# TODO: really a good idea to bootstrap if upgrade fails?
 flask db upgrade \
     || ( flask fab create-db \
         && flask fab create-admin --username admin --firstname admin --lastname admin --email admin@matheworkout.at --password password )
