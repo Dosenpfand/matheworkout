39787d5f4924159b599704a101293c7f69d04d72
Dosenpfand <git@sad.bz>
2024-10-20 20:33:41 +0200

    Improve

--- prestart.sh
+++ prestart.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 
-# TODO: really a good idea to bootstrap if upgrade fails?
-flask db upgrade \
-    || ( flask fab create-db \
-        && flask fab create-admin --username admin --firstname admin --lastname admin --email admin@matheworkout.at --password password )
+if flask db current | grep "(head)"; then
+    echo "Running database upgrade."
+    flask db upgrade
+else
+    echo "Initizalizing database."
+    flask fab create-db
+    flask db stamp
+    # TODO: Fix! user creation functionality broken!
+    flask fab create-admin \
+        --username admin \
+        --firstname admin \
+        --lastname admin \
+        --email admin@matheworkout.at \
+        --password ${FLASK_ADMIN_PASSWORD:-$(tr -dc A-Za-z0-9 </dev/urandom | head -c 20; echo)}
+fi
